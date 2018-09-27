1ee21247cc0d49382a3e2d5fe53a713447bcc1c4
Samuel Beck <sam@acst.com>
2018-09-27 09:29:14 -0700

    Initial Commit

---
+++ src/main/java/com/samueljbeck/robototextviews/RobotoEditTextBackSpaceDetection.java
@@ -0,0 +1,69 @@
+package com.samueljbeck.robototextviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputConnectionWrapper;
+
+import java.util.Random;
+
+/**
+ * Created by samuelbeck on 11/21/16.
+ */
+public class RobotoEditTextBackSpaceDetection extends RobotoEditText {
+
+    private Random r = new Random();
+
+    public RobotoEditTextBackSpaceDetection(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public RobotoEditTextBackSpaceDetection(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RobotoEditTextBackSpaceDetection(Context context) {
+        super(context);
+    }
+
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        return new ZanyInputConnection(super.onCreateInputConnection(outAttrs),
+                true);
+    }
+
+    private class ZanyInputConnection extends InputConnectionWrapper {
+
+        public ZanyInputConnection(InputConnection target, boolean mutable) {
+            super(target, mutable);
+        }
+
+        @Override
+        public boolean sendKeyEvent(KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN
+                    && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
+                // Un-comment if you wish to cancel the backspace:
+                // return false;
+            }
+            return super.sendKeyEvent(event);
+        }
+
+
+        @Override
+        public boolean deleteSurroundingText(int beforeLength, int afterLength) {
+            // magic: in latest Android, deleteSurroundingText(1, 0) will be called for backspace
+            if (beforeLength == 1 && afterLength == 0) {
+                // backspace
+                return sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL))
+                        && sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
+            }
+
+            return super.deleteSurroundingText(beforeLength, afterLength);
+        }
+
+    }
+
+}
